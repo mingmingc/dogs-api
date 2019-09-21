8a681c63e3ee2f631cfe7e927a2a009f563e1736
mingmingc <caressi.m@gmail.com>
2019-09-21 13:08:29 -0700

    completed get statement

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,4 +3,4 @@ USE dogs_db;
 INSERT INTO dogs (name, age, breed, owner_name) VALUES
 ("baobao", 14, "mixed", "mingming"), ("mishka", 12, "mixed", "john"), 
 ("buddy", 5, "german shepard", "mingming"), ("colbie", 12, "mixed", "lynn"), ("casper", 1, "bernese", "mingming"), ("ray", 2, "daschund", "maddy"),
-("barry", 0, "daschund", "maddy"), ("louis", 6, "goldie", "steph"), ("shira", 3, "pitbull", "joe"), ("riley", 10, "poodle", "dana");
+("barry", 0, "daschund", "maddy"), ("louis", 6, "goldie", "steph"), ("shira", 3, "pitbull", "joe");
--- server.js
+++ server.js
@@ -19,113 +19,21 @@ app.use(express.static("public"));
 
 app.use(bodyParser.json());
 
-app.get('/cats', function (req, res) {
+app.get('/dogs', function(req, res) {
 	const age = req.query.age;
-	const ageIsValid = checkAge(age);
-	console.log(age)
-	if (age && !ageIsValid) {
-		res.status(400);
-		res.send("Please provide age in number format (e.g. 5, not 'five')");
-		return;
-	}
+	const breed = req.query.breed;
 
-	const filter = age ? " WHERE age > " + age : "";
-	connection.query("SELECT * FROM cats" + filter, function (err, results, fields) {
+	const ageFilter = age ? " WHERE age > " + age : "";
+	const breedFilter = !age && breed ? " WHERE breed = '" + breed + "'": "";
+	const breedFilter2 = age && breed ? " AND breed = '" + breed + "'" : "";
+	connection.query("SELECT * FROM dogs" + ageFilter + breedFilter + breedFilter2, function(err, results) {
 		if (err) throw err;
 		res.json(results);
 	})
-});
-
-app.get('/cats/:id', function (req, res) {
-	const id = req.params.id;
-	const idIsValid = checkId(id);
-	if (!idIsValid) {
-		res.status(404);
-		res.send("Cat not found!")
-		return;
-	}
-	connection.query("SELECT * FROM cats WHERE id = " + id, function (err, results, fields) {
-		if (err) throw err;
-		res.json(results);
-	})
-});
-
-app.post('/cats', function (req, res) {
-	console.log(req.body)
-	const name = req.body.name;
-	const age = req.body.age;
-	const ageIsValid = checkAge(age);
-	const nameIsValid = checkName(name);
-
-	if (!ageIsValid) {
-		res.status(400);
-		res.send("Please provide age in number format (e.g. 5, not 'five')");
-		return;
-	} else if (!nameIsValid) {
-		res.status(400);
-		res.send("Please name your cat!");
-		return;
-	}
-	connection.query("INSERT INTO cats (name, age) VALUES ('" + name + "', " + age + ")", function (err, results, fields) {
-		if (err) throw err;
-		res.json(results);
-	})
-});
-
-
-app.delete('/cats/:id', function (req, res) {
-	const id = req.params.id;
-	const idIsValid = checkId(id);
-	if (!idIsValid) {
-		res.status(404);
-		res.send("Cat not found!")
-		return;
-	}
-
-	connection.query("DELETE FROM cats WHERE id = " + id, function (err, results, fields) {
-		if (err) throw err;
-		res.json(results);
-	})
-});
-
-app.put('/cats/:id', function (req, res) {
-	const id = req.params.id;
-	const age = req.body.age;
-	const idIsValid = checkId(id);
-	const ageIsValid = checkAge(age);
-	if (!ageIsValid) {
-		res.status(400);
-		res.send("Please provide age in number format (e.g. 5, not 'five')");
-		return;
-	}
-
-	if (!idIsValid) {
-		res.status(404);
-		res.send("Cat not found!")
-		return;
-	}
-
-	connection.query("UPDATE cats SET age = " + age + " WHERE id = " + id, function (err, results, fields) {
-		if (err) throw err;
-		res.json(results);
-	})
-});
+})
 
 app.listen(3001, function () {
 	console.log('listening on 3001');
 });
-
-function checkAge(age) {
-	return !(isNaN(age) || age.length === 0);
-}
-
-function checkId(id) {
-	return !(isNaN(id) || id.length === 0);
-}
-
-function checkName(name) {
-	return !(name.length === 0);
-
-}
 
 
